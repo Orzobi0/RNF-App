ef5119e84e9c9265f661a1774dba48f9195a70c8
mariogs <gonzalezsorianomario@gmail.com>
2025-06-20 12:45:25 +0200

    linea de puntos consecutiva

--- src/components/chartElements/ChartLine.jsx
+++ src/components/chartElements/ChartLine.jsx
@@ -1,31 +1,25 @@
 import React from 'react';
     import { motion } from 'framer-motion';
 
-    const ChartLine = ({ data, allDataPoints, getX, getY, temperatureField = 'temperature' }) => {
-      if (!data || data.length < 2) return null;
+const ChartLine = ({ data, allDataPoints, getX, getY, temperatureField = 'temperature' }) => {
+  if (!data || data.length < 2) return null;
 
-      let pathD = "M";
-      let firstPointIndex = -1;
+  let pathD = '';
+  let lastValidIndex = null;
 
-      allDataPoints.forEach((point, index) => {
-        const dataPoint = data.find(dp => dp.id === point.id);
-        if (dataPoint && dataPoint[temperatureField] !== null && dataPoint[temperatureField] !== undefined && !dataPoint.ignored) {
-          const x = getX(index);
-          const y = getY(dataPoint[temperatureField]);
-          if (firstPointIndex === -1) {
-            pathD += `${x} ${y}`;
-            firstPointIndex = index;
-          } else {
-            pathD += ` L ${x} ${y}`;
-          }
-        } else if (firstPointIndex !== -1 && point[temperatureField] === null && !point.ignored) {
-           pathD += ` M`; 
-           firstPointIndex = -1;
-        } else if (firstPointIndex !== -1 && point.ignored) {
-           pathD += ` M`;
-           firstPointIndex = -1;
-        }
-      });
+  allDataPoints.forEach((point, index) => {
+    const dataPoint = data.find(dp => dp.id === point.id);
+    if (dataPoint && dataPoint[temperatureField] !== null && dataPoint[temperatureField] !== undefined && !dataPoint.ignored) {
+      const x = getX(index);
+      const y = getY(dataPoint[temperatureField]);
+      if (lastValidIndex !== null && index === lastValidIndex + 1) {
+        pathD += ` L ${x} ${y}`;
+      } else {
+        pathD += `${pathD ? ' ' : ''}M ${x} ${y}`;
+      }
+      lastValidIndex = index;
+    }
+  });
       
       if (!pathD.includes("L")) return null;
 
