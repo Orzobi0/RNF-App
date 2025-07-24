258d5c528486281a5aa14118b01fb3a25535f7ab
mariogs <gonzalezsorianomario@gmail.com>
2025-07-24 15:10:37 +0200

    PWA sin png

--- src/main.jsx
+++ src/main.jsx
@@ -4,9 +4,16 @@
     import App from '@/App';
     import '@/index.css';
 
-    ReactDOM.createRoot(document.getElementById('root')).render(
-      <React.StrictMode>
-        <App />
-      </React.StrictMode>
-    );
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    const swUrl = `${import.meta.env.BASE_URL}sw.js`;
+    navigator.serviceWorker.register(swUrl);
+  });
+}
   
