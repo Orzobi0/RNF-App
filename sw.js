9438c9b5ffbd862556a766130cdbe019ffb77c6b
Orzobi0 <Orzobi0@users.noreply.github.com>
2025-09-27 15:11:52 +0000

    deploy: 3156a1ab450ee83edd156cdf29ba6bd09bc2299d

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = '2025-09-24T07:27:30.869Z';
+const CACHE_VERSION = '2025-09-27T15:11:51.589Z';
 const CACHE_NAME = `rnf-app-cache-${CACHE_VERSION}`;
 const BASE_URL = self.registration.scope;
 const ASSETS = [
@@ -11,24 +11,49 @@ const ASSETS = [
   `${BASE_URL}apple-touch-icon.png`
 ];
 // Assets generated during the build step will be injected into this array.
-const BUILD_ASSETS = (["assets/ChartTooltip-5350e5f6.js","assets/DataEntryForm-99673c61.js","assets/DeletionDialog-1303d971.js","assets/OverlapWarningDialog-7190f3a2.js","assets/badge-fc139165.js","assets/dialog-9607b487.js","assets/eye-67758e41.js","assets/eye-off-1c06221e.js","assets/input-235c5ced.js","assets/label-0ecf343f.js","assets/plus-1f07c869.js","assets/useCycleData-af0f9bf4.js","assets/index-d9964085.css","assets/index-752021ba.js","assets/ArchivedCyclesPage-a5d2d81d.js","assets/AuthPage-f6c2c8fa.js","assets/ChartPage-4342ff1a.js","assets/CycleDetailPage-ff26e289.js","assets/DashboardPage-87b15230.js","assets/RecordsPage-01e062cc.js","assets/SettingsPage-01be1fad.js"] || []).map(
+const BUILD_ASSETS = (["assets/ChartTooltip-086fab25.js","assets/DeletionDialog-8f61e9be.js","assets/OverlapWarningDialog-3f38b600.js","assets/badge-e36f7fd8.js","assets/computePeakStatuses-a8c393d1.js","assets/eye-61791957.js","assets/eye-off-b85355d3.js","assets/input-2a7ce825.js","assets/label-e033e1bb.js","assets/select-4e9e95be.js","assets/trash-2-f8d41441.js","assets/useCycleData-4efbc629.js","assets/index-a18e6b05.css","assets/index-24043ae7.js","assets/index.es-ed0e0bd7.js","assets/purify.es-2de9db7f.js","assets/html2canvas.esm-e0a7d97b.js","assets/ArchivedCyclesPage-47966f21.js","assets/AuthPage-aa27ad5a.js","assets/ChartPage-659a9c9a.js","assets/CycleDetailPage-0ba9d12c.js","assets/DashboardPage-1afa71ad.js","assets/RecordsPage-3ba03bcd.js","assets/SettingsPage-9536063a.js"] || []).map(
   (asset) => `${BASE_URL}${asset}`
 );
 
 self.addEventListener('install', event => {
+  console.log('SW: Installing new version', CACHE_VERSION);
   event.waitUntil(
-    caches.open(CACHE_NAME)
+    caches
+      .open(CACHE_NAME)
       .then(cache => cache.addAll([...ASSETS, ...BUILD_ASSETS]))
-      .then(() => self.skipWaiting())
+      .then(() => {
+        console.log('SW: Cache populated, skipping waiting');
+        return self.skipWaiting();
+      })
   );
 });
 
 self.addEventListener('activate', event => {
+  console.log('SW: Activating version', CACHE_VERSION);
   event.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.map(key => key !== CACHE_NAME && caches.delete(key)))
-    ).then(() => self.clients.claim())
+    caches
+      .keys()
+      .then(keys =>
+        Promise.all(
+          keys
+            .filter(key => key !== CACHE_NAME)
+            .map(key => {
+              console.log('SW: Deleting old cache', key);
+              return caches.delete(key);
+            })
+        )
+      )
+      .then(() => {
+        console.log('SW: Claiming clients');
+        return self.clients.claim();
+      })
   );
+});
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    console.log('SW: Received SKIP_WAITING message');
+    self.skipWaiting();
+  }
 });
 
 self.addEventListener('fetch', (event) => {
