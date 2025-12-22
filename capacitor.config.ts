a3bb3fb6fe821096f5669560ed89f0cac49d19c1
mariogs <gonzalezsorianomario@gmail.com>
2025-12-22 13:23:04 +0100

    carpeta android app

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.fertiliapp.fertiliapp',
+  appName: 'FertiliApp',
+  webDir: 'dist'
+};
+
+export default config;
